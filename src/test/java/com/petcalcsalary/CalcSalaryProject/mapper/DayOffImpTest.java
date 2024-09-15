2f4274a438dd94ae770ff4784fa7b8f912b9487d
mxnv <vadim.beller@mail.ru>
2024-09-15 14:07:57 +0300

    final unit tests in DayOffImp

--- src/test/java/com/petcalcsalary/CalcSalaryProject/mapper/DayOffImpTest.java
+++ src/test/java/com/petcalcsalary/CalcSalaryProject/mapper/DayOffImpTest.java
@@ -1,6 +1,7 @@
 package com.petcalcsalary.CalcSalaryProject.mapper;
 
 import com.petcalcsalary.CalcSalaryProject.ConstantsClassTest;
+import com.petcalcsalary.CalcSalaryProject.others.ConstantsClass;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -49,7 +50,7 @@ public class DayOffImpTest {
     }
 
     @Test
-    void checkLocalDateIsFestiveDays(){
+    void compareLocalDateWithUsualDayOffTest(){
         for (int i = 1; i <= 7; i++){
             if (i == 7 || i == 1) {
                 Assertions.assertTrue(dayOff.compareLocalDateWithUsualDayOff(i));
@@ -57,6 +58,28 @@ public class DayOffImpTest {
                 Assertions.assertFalse(dayOff.compareLocalDateWithUsualDayOff(i));
             }
         }
+    }
+
+    @Test
+    void checkLocalDateIsFestiveDaysTest(){
+        Optional<LocalDate> optionalLocalDate = dayOff.checkLocalDateIsFestiveDays(ConstantsClassTest.END_DATE);
+        if (optionalLocalDate.isEmpty()){
+            Assertions.assertFalse(false);
+        }
+        optionalLocalDate = dayOff.checkLocalDateIsFestiveDays(ConstantsClassTest.START_DATE);
+        if (optionalLocalDate.isPresent()) {
+            Assertions.assertTrue(true);
+        }
+    }
+
+    @Test
+    void getDatesWithoutUsualDayOffTest(){
+        List<LocalDate> dateList = dayOff.getDatesBetween(ConstantsClassTest.START_DATE, ConstantsClassTest.END_DATE);
+        Assertions.assertNotEquals(dateList.size(), dayOff.getDatesWithoutUsualDayOff(dateList).size());
+        //
+        dateList.clear();
+        dateList.add(ConstantsClassTest.START_DATE.minusDays(ConstantsClass.ONE_FLAG));
+        Assertions.assertEquals(dateList.size(), dayOff.getDatesWithoutUsualDayOff(dateList).size());
     }
 
 }
